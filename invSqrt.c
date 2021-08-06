344b9a91f3cd9e7dcff9b6e165fd99123c4c3dd3
Alexlin7 <alitw@outlook.com>
2021-08-06 22:13:24 +0800

    new update

---
+++ invSqrt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+float invSqrt(float x){
+    int i=*(int*)&x;
+    i=1597463007-(i>>1);
+    x=*(float*)&i;
+    return x;
+}
+
+int main(){
+    float x=0,ans1=0,ans2=0;
+    printf("Enter X:");
+    scanf("%f",&x);
+    ans1=invSqrt(x);
+    printf("\ninvSqrt is %f\n",ans1);
+    return 0;
+    
+
+}
