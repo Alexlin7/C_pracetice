48cc217000dabc11037dc9b61131a6cdfb096733
Alexlin7 <alitw@outlook.com>
2021-08-10 01:12:47 +0800

    修改樣式

--- invSqrt.c
+++ invSqrt.c
@@ -2,19 +2,17 @@
 
 
 float invSqrt(float x){
-    int i=*(int*)&x;
-    i=1597463007-(i>>1);    //I don't know.
-    x=*(float*)&i;
+    int i = *(int*)&x;
+    i = 1597463007 - (i >> 1);    //I don't know.
+    x = *(float*)&i;
     return x;
 }
 
 int main(){
-    float x=0,ans1=0,ans2=0;
+    float x = 0 , ans1 = 0 , ans2 = 0;
     printf("Enter X:");
     scanf("%f",&x);
-    ans1=invSqrt(x);
+    ans1 = invSqrt(x);
     printf("\ninvSqrt is %f\n",ans1);
-    return 0;
-    
-
+    return 0; 
 }
